a7e27f5d7c07b905822fb748ce881f1dce618a80
BoussonKarel <bousson.karel@gmail.com>
2022-03-03 16:54:41 +0100

    move workers to app/workers

--- index.js
+++ index.js
@@ -4,25 +4,61 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-var Funnel = require('broccoli-funnel');
+const esbuild = require('esbuild');
 
-const { buildWorkers } = require('./lib/worker-build');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: require('./package').name,
 
-  treeForPublic(tree) {
-    let buildDir = fs.mkdtempSync(
-      path.join(os.tmpdir(), 'ember-promise-worker')
-    );
+  included(app) {
+    this.appRoot = path.join(app.project.root);
+    this.workerRoot = path.join(this.appRoot, 'app', 'workers')
+  },
+
+  treeForPublic() {
+    const workers = this._detectWorkers();
+
+    const buildDir = fs.mkdtempSync(path.join(os.tmpdir(), 'ember-promise-worker-'));
 
     let options = {
       isProduction: true,
       buildDir,
     };
 
-    buildWorkers(options);
+    let workerBuilder = this._configureWorkerBuilder(options);
 
-    return new Funnel(buildDir, { destDir: 'workers/' });
+    Object.entries(workers).map(workerBuilder);
+
+    return new Funnel(buildDir, { destDir: 'workers' })
   },
+
+  _configureWorkerBuilder({ isProduction, buildDir }) {
+    return ([name, entryPath]) => {
+      esbuild.buildSync({
+        loader: { '.ts': 'ts' },
+        entryPoints: [entryPath],
+        bundle: true,
+        outfile: path.join(buildDir, `${name}.js`), // {buildDir}/workers/{name}.js
+        format: 'esm',
+        minify: isProduction,
+        sourcemap: !isProduction,
+        tsconfig: path.join(this.appRoot, 'tsconfig.json'),
+      });
+    };
+  },
+
+  _detectWorkers() {
+    let workers = {};
+    let dir = fs.readdirSync(this.workerRoot);
+
+    for (let i = 0; i < dir.length; i++) {
+      let name = dir[i];
+
+      workers[name] = path.join(this.workerRoot, name, 'index.ts');
+    }
+
+    return workers;
+  }
 };
