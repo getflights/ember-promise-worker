5fac5038aa6cdf9c6af785fef6cc5ff2184dfc3f
BoussonKarel <bousson.karel@gmail.com>
2022-03-25 14:05:47 +0100

    beta.2

--- tests/unit/services/workers-test.ts
+++ tests/unit/services/workers-test.ts
@@ -2,41 +2,58 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import WorkersService from 'ember-promise-worker/services/workers';
 
-module('Unit | Service | workers', function(hooks) {
+module('Unit | Service | workers', function (hooks) {
   setupTest(hooks);
 
-  test('workers service works', async function(assert) {
-    let workers: WorkersService = this.owner.lookup('service:workers') as WorkersService;
+  test('workers service works', async function (assert) {
+    let workers: WorkersService = this.owner.lookup(
+      'service:workers'
+    ) as WorkersService;
 
     // Testworker exists in this addon
-    let testworker = await workers._getWorker('testworker')
-    assert.ok(testworker, 'testworker exists')
+    let testworker = await workers._getWorker('testworker');
+    assert.ok(testworker, 'testworker exists');
 
     // Worker just returns his messages
-    const testMsg = "testmessage"
-    const workerMsg = await workers.workerMessage('testworker', testMsg)
-    assert.ok(testMsg === workerMsg, 'testworker returns your message')
-  })
+    const testMsg = 'testmessage';
+    const workerMsg = await workers.workerMessage(
+      'testworker',
+      // @ts-ignore
+      testMsg
+    );
+    assert.ok(testMsg === workerMsg, 'testworker returns your message');
+  });
 
-  test('workers service doesn\'t find non existing one', async function(assert) {
-    let workers: WorkersService = this.owner.lookup('service:workers') as WorkersService;
-    assert.ok(workers, 'workers service was found')
+  test("workers service doesn't find non existing one", async function (assert) {
+    let workers: WorkersService = this.owner.lookup(
+      'service:workers'
+    ) as WorkersService;
+    assert.ok(workers, 'workers service was found');
 
-    let hasInitErrors = await workers._getWorker('zkHlELpdsqRsAXAL6u9q').then(() => {
-      return false
-    }).catch(() => {
-      return true
-    })
+    let hasInitErrors = await workers
+      ._getWorker('zkHlELpdsqRsAXAL6u9q')
+      .then(() => {
+        return false;
+      })
+      .catch(() => {
+        return true;
+      });
 
-    assert.ok(hasInitErrors, 'getWorker throws an error')
+    assert.ok(hasInitErrors, 'getWorker throws an error');
 
-    let hasMessageErrors = await workers.workerMessage('zkHlELpdsqRsAXAL6u9q', 'this shouldnt work').then(() => {
-      return false
-    }).catch(() => {
-      return true
-    })
+    let hasMessageErrors = await workers
+      .workerMessage(
+        'zkHlELpdsqRsAXAL6u9q',
+        // @ts-ignore
+        'this shouldnt work'
+      )
+      .then(() => {
+        return false;
+      })
+      .catch(() => {
+        return true;
+      });
 
-    assert.ok(hasMessageErrors, 'workerMessage throws an error')
-  })
+    assert.ok(hasMessageErrors, 'workerMessage throws an error');
+  });
 });
-
