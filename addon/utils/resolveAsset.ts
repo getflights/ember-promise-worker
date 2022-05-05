cd1b249eee5c63c490ed1b946de383156b627e92
BoussonKarel <bousson.karel@gmail.com>
2022-05-05 11:10:29 +0200

    fix asset resolving using assetMap

--- addon/utils/resolveAsset.ts
+++ addon/utils/resolveAsset.ts
@@ -1,28 +1,70 @@
 import DEBUG from '@glimmer/env';
 
-let assetMap: {
+interface AssetMap {
+  loaded: boolean;
   assets: {
     [key: string]: string;
-  };
-  prepend: string;
+  } | null;
+  prepend: string | null;
+  enabled: boolean | null;
+}
+
+let assetMap: AssetMap = {
+  loaded: false,
+  assets: null,
+  prepend: null,
+  enabled: null,
 };
 
 // More errors like checking the config
 // https://github.com/buschtoens/ember-cli-resolve-asset
 
+let promise: Promise<AssetMap>;
+
+export function loadAssetMap() {
+  if (promise) {
+    return promise;
+  }
+  promise = _fetchAssetMap();
+  return promise;
+}
+
+async function _fetchAssetMap(): Promise<AssetMap> {
+  // const assetMapPath = ;
+
+  // if (assetMapPath) {
+  assetMap.enabled = true;
+  assetMap = await fetch('/assets/assetMap.json')
+    .then((r) => r.json())
+    .catch(() => {
+      // if (DEBUG) {
+      //   throw new Error('Asset map not found');
+      // }
+      assetMap.enabled = false;
+    });
+  // } else {
+  //   assetMap.enabled = false;
+  // }
+
+  return assetMap;
+}
+
 export const resolveAsset = async (path: string) => {
-  // fetch asset map
-  if (!assetMap) {
-    assetMap = await fetch('/assets/assetMap.json')
-      .then((r) => r.json())
-      .catch(() => {
-        if (DEBUG) {
-          throw new Error('Asset map not found')
-        }
-      });
+  if (!assetMap.assets) {
+    await loadAssetMap();
   }
 
-  path = path.replace(/^\//g, "") // remove leading '/' just in case
+  if (assetMap.enabled === false) {
+    return path;
+  }
 
-  return assetMap.assets[path];
+  path = path.replace(/^\//g, ''); // remove leading '/' just in case
+
+  if (!assetMap.assets![path]) {
+    if (DEBUG) {
+      throw new Error(`Asset '${path}' was not found in the asset map.`);
+    }
+  }
+
+  return assetMap.assets![path];
 };
